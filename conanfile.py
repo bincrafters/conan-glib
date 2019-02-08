9df3ad04afbf9c31598cc92c85e77eac369ac357
SSE4 <tomskside@gmail.com>
2019-02-08 23:55:10 +0700

    - bump to 2.58.3
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ import os
 
 class GLibConan(ConanFile):
     name = "glib"
-    version = "2.57.1"
+    version = "2.58.3"
     description = "GLib provides the core application building blocks for libraries and applications written in C"
     url = "https://github.com/bincrafters/conan-glib"
     homepage = "https://github.com/GNOME/glib"
