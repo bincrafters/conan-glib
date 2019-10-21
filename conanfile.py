247fa186a9b1ff0737938163e990b62334d7c10a
ericLemanissier <ericLemanissier@users.noreply.github.com>
2019-10-21 17:17:34 +0200

    migrate meson to 0.52.0 on CCI

--- conanfile.py
+++ conanfile.py
@@ -72,7 +72,7 @@ class GLibConan(ConanFile):
 
     def build_requirements(self):
         if not tools.which("meson"):
-            self.build_requires("meson_installer/0.50.0@bincrafters/stable")
+            self.build_requires("meson/0.52.0")
         if not tools.which("pkg-config"):
             self.build_requires("pkg-config_installer/0.29.2@bincrafters/stable")
 
