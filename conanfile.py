77a077f5aca7be54481c90a32fd656c229892b96
Croydon <git@cr0ydon.com>
2020-01-12 15:05:39 +0100

    Try to fix meson option value

--- conanfile.py
+++ conanfile.py
@@ -81,7 +81,7 @@ class GLibConan(ConanFile):
         if tools.is_apple_os(self.settings.os):
             defs["iconv"] = "native"  # https://gitlab.gnome.org/GNOME/glib/issues/1557
         if self.settings.os == "Linux":
-            defs["selinux"] = self.options.with_selinux
+            defs["selinux"] = "enabled" if self.options.with_selinux else "disabled"
             defs["libmount"] = self.options.with_mount
         defs["internal_pcre"] = not self.options.with_pcre
 
