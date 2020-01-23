51a2656534038d9b0092d42e4fc35c2a9e4b0d27
Kristoffer Ellersgaard Koch <kristoffer.koch@gmail.com>
2020-01-23 09:45:50 +0100

    tests: Add casts to make it possible to compile with gcc9

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -10,9 +10,9 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    g_queue_push_tail(queue, "Alice");
-    g_queue_push_tail(queue, "Bob");
-    g_queue_push_tail(queue, "Fred");
+    g_queue_push_tail(queue, const_cast<char *>("Alice"));
+    g_queue_push_tail(queue, const_cast<char *>("Bob"));
+    g_queue_push_tail(queue, const_cast<char *>("Fred"));
 
     printf("head: %s\n", (const char*) g_queue_peek_head(queue));
     printf("tail: %s\n", (const char*) g_queue_peek_tail(queue));
@@ -20,7 +20,7 @@ int main(int argc, char** argv) {
     printf("pop: %s\n", (const char*) g_queue_pop_head(queue));
     printf("length: %d\n",  g_queue_get_length(queue));
     printf("head: %s\n", (const char*) g_queue_peek_head(queue));
-    g_queue_push_head(queue, "Big Jim");
+    g_queue_push_head(queue, const_cast<char *>("Big Jim"));
     printf("length: %d\n",  g_queue_get_length(queue));
     printf("head: %s\n", (const char*) g_queue_peek_head(queue));
     g_queue_free(queue);
