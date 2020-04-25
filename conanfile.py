f370e7f0f62bd9db1f75d839194ec308697ed0ef
bincrafters-user <bincrafters@gmail.com>
2020-04-25 04:44:32 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.24.0

--- conanfile.py
+++ conanfile.py
@@ -49,7 +49,7 @@ class GLibConan(ConanFile):
             del self.options.with_selinux
 
     def build_requirements(self):
-        self.build_requires("meson/0.53.2")
+        self.build_requires("meson/0.54.0")
         if not tools.which("pkg-config"):
             self.build_requires("pkg-config_installer/0.29.2@bincrafters/stable")
 
